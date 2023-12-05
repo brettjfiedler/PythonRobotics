6f3b54bae51b7e59019d22f2074925f951b9b591
bfied <bfiedler313@gmail.com>
2023-12-04 20:58:57 -0800

    first commit

--- Swarm/dynamic_window_approach.py
+++ Swarm/dynamic_window_approach.py
@@ -1,4 +1,11 @@
-#initialize simulation
+"""
+
+Mobile robot motion planning sample with Dynamic Window Approach
+
+author: Atsushi Sakai (@Atsushi_twi), Göktuğ Karakaşlı
+
+"""
+
 import math
 from enum import Enum
 
@@ -8,6 +15,20 @@ import numpy as np
 show_animation = True
 
 
+def dwa_control(x, config, goal, ob):
+    """
+    Dynamic Window Approach control
+    """
+    dw = calc_dynamic_window(x, config)
+
+    u, trajectory = calc_control_and_trajectory(x, dw, config, goal, ob)
+
+    return u, trajectory
+
+
+class RobotType(Enum):
+    circle = 0
+    rectangle = 1
 
 
 class Config:
@@ -30,10 +51,15 @@ class Config:
         self.speed_cost_gain = 1.0
         self.obstacle_cost_gain = 1.0
         self.robot_stuck_flag_cons = 0.001  # constant to prevent robot stucked
+        self.robot_type = RobotType.circle
 
+        # if robot_type == RobotType.circle
+        # Also used to check if goal is reached in both types
+        self.robot_radius = 1.0  # [m] for collision check
+
+        # if robot_type == RobotType.rectangle
         self.robot_width = 0.5  # [m] for collision check
         self.robot_length = 1.2  # [m] for collision check
-
         # obstacles [x(m) y(m), ....]
         self.ob = np.array([[-1, -1],
                             [0, 2],
@@ -52,21 +78,36 @@ class Config:
                             [13.0, 13.0]
                             ])
 
+    @property
+    def robot_type(self):
+        return self._robot_type
+
+    @robot_type.setter
+    def robot_type(self, value):
+        if not isinstance(value, RobotType):
+            raise TypeError("robot_type must be an instance of RobotType")
+        self._robot_type = value
+
+
 config = Config()
 
 
-def dwa_control(x, config, goal, ob):
+def motion(x, u, dt):
     """
-    Dynamic Window Approach control
+    motion model
     """
-    dw = calc_dynamic_window(x, config)
 
-    u, trajectory = calc_control_and_trajectory(x, dw, config, goal, ob)
+    x[2] += u[1] * dt
+    x[0] += u[0] * math.cos(x[2]) * dt
+    x[1] += u[0] * math.sin(x[2]) * dt
+    x[3] = u[0]
+    x[4] = u[1]
 
-    return u, trajectory
+    return x
 
 
 def calc_dynamic_window(x, config):
+
     """
     calculation dynamic window based on current state x
     """
@@ -86,6 +127,22 @@ def calc_dynamic_window(x, config):
           max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
 
     return dw
+
+
+def predict_trajectory(x_init, v, y, config):
+    """
+    predict trajectory with an input
+    """
+
+    x = np.array(x_init)
+    trajectory = np.array(x)
+    time = 0
+    while time <= config.predict_time:
+        x = motion(x, [v, y], config.dt)
+        trajectory = np.vstack((trajectory, x))
+        time += config.dt
+
+    return trajectory
 
 
 def calc_control_and_trajectory(x, dw, config, goal, ob):
@@ -122,25 +179,86 @@ def calc_control_and_trajectory(x, dw, config, goal, ob):
                     # best omega=0 rad/s (heading to the goal with
                     # angle difference of 0)
                     best_u[1] = -config.max_delta_yaw_rate
-
     return best_u, best_trajectory
 
 
-
-def motion(x, u, dt):
+def calc_obstacle_cost(trajectory, ob, config):
     """
-    motion model
+    calc obstacle cost inf: collision
+    """
+    ox = ob[:, 0]
+    oy = ob[:, 1]
+    dx = trajectory[:, 0] - ox[:, None]
+    dy = trajectory[:, 1] - oy[:, None]
+    r = np.hypot(dx, dy)
+
+    if config.robot_type == RobotType.rectangle:
+        yaw = trajectory[:, 2]
+        rot = np.array([[np.cos(yaw), -np.sin(yaw)], [np.sin(yaw), np.cos(yaw)]])
+        rot = np.transpose(rot, [2, 0, 1])
+        local_ob = ob[:, None] - trajectory[:, 0:2]
+        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+        local_ob = np.array([local_ob @ x for x in rot])
+        local_ob = local_ob.reshape(-1, local_ob.shape[-1])
+        upper_check = local_ob[:, 0] <= config.robot_length / 2
+        right_check = local_ob[:, 1] <= config.robot_width / 2
+        bottom_check = local_ob[:, 0] >= -config.robot_length / 2
+        left_check = local_ob[:, 1] >= -config.robot_width / 2
+        if (np.logical_and(np.logical_and(upper_check, right_check),
+                           np.logical_and(bottom_check, left_check))).any():
+            return float("Inf")
+    elif config.robot_type == RobotType.circle:
+        if np.array(r <= config.robot_radius).any():
+            return float("Inf")
+
+    min_r = np.min(r)
+    return 1.0 / min_r  # OK
+
+
+def calc_to_goal_cost(trajectory, goal):
+    """
+        calc to goal cost with angle difference
     """
 
-    x[2] += u[1] * dt
-    x[0] += u[0] * math.cos(x[2]) * dt
-    x[1] += u[0] * math.sin(x[2]) * dt
-    x[3] = u[0]
-    x[4] = u[1]
+    dx = goal[0] - trajectory[-1, 0]
+    dy = goal[1] - trajectory[-1, 1]
+    error_angle = math.atan2(dy, dx)
+    cost_angle = error_angle - trajectory[-1, 2]
+    cost = abs(math.atan2(math.sin(cost_angle), math.cos(cost_angle)))
 
-    return x
+    return cost
 
-def main(gx=10.0, gy=10.0):
+
+def plot_arrow(x, y, yaw, length=0.5, width=0.1):  # pragma: no cover
+    plt.arrow(x, y, length * math.cos(yaw), length * math.sin(yaw),
+              head_length=width, head_width=width)
+    plt.plot(x, y)
+
+
+def plot_robot(x, y, yaw, config):  # pragma: no cover
+    if config.robot_type == RobotType.rectangle:
+        outline = np.array([[-config.robot_length / 2, config.robot_length / 2,
+                             (config.robot_length / 2), -config.robot_length / 2,
+                             -config.robot_length / 2],
+                            [config.robot_width / 2, config.robot_width / 2,
+                             - config.robot_width / 2, -config.robot_width / 2,
+                             config.robot_width / 2]])
+        Rot1 = np.array([[math.cos(yaw), math.sin(yaw)],
+                         [-math.sin(yaw), math.cos(yaw)]])
+        outline = (outline.T.dot(Rot1)).T
+        outline[0, :] += x
+        outline[1, :] += y
+        plt.plot(np.array(outline[0, :]).flatten(),
+                 np.array(outline[1, :]).flatten(), "-k")
+    elif config.robot_type == RobotType.circle:
+        circle = plt.Circle((x, y), config.robot_radius, color="b")
+        plt.gcf().gca().add_artist(circle)
+        out_x, out_y = (np.array([x, y]) +
+                        np.array([np.cos(yaw), np.sin(yaw)]) * config.robot_radius)
+        plt.plot([x, out_x], [y, out_y], "-k")
+
+
+def main(gx=10.0, gy=10.0, robot_type=RobotType.circle):
     print(__file__ + " start!!")
     # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
     x = np.array([0.0, 0.0, math.pi / 8.0, 0.0, 0.0])
@@ -148,20 +266,14 @@ def main(gx=10.0, gy=10.0):
     goal = np.array([gx, gy])
 
     # input [forward speed, yaw_rate]
+
+    config.robot_type = robot_type
     trajectory = np.array(x)
-
     ob = config.ob
-
-
     while True:
-        # call the dynamic window control algorithm using dynamic window control
         u, predicted_trajectory = dwa_control(x, config, goal, ob)
-
-        #simulate the agent motion using x and u
-        x = motion(x, u, config.dt)
-
-        #store the state history of agent
-        trajectory = np.vstack((trajectory, x))
+        x = motion(x, u, config.dt)  # simulate robot
+        trajectory = np.vstack((trajectory, x))  # store state history
 
         if show_animation:
             plt.cla()
@@ -191,6 +303,7 @@ def main(gx=10.0, gy=10.0):
         plt.pause(0.0001)
         plt.show()
 
-if __name__ == '__main__':
-    print('hello_world')
 
+if __name__ == '__main__':
+    main(robot_type=RobotType.rectangle)
+    # main(robot_type=RobotType.circle)
